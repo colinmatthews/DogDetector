5fdcd3e8c1063dba2cdbf567f17c0113f43f7723
colinmatthews <matth11f@uwindsor.ca>
2018-01-11 18:32:51 -0500

    Add view and logic

--- app.js
+++ app.js
@@ -1,2 +1,61 @@
-const Clarifai = require('clarifai');
 const express = require('express');
+const Clarifai = require('clarifai');
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.route('/')
+    .get(function (req, res) {
+        const url = "https://samples.clarifai.com/puppy.jpeg";
+        ClarifaiApp.models.predict(Clarifai.GENERAL_MODEL,url).then(
+            function(response) {
+                let message = "It's not a dog";
+                console.log(response);
+                let concepts = response.outputs[0].data.concepts;
+                for(let i = 0; i < concepts.length; i++){
+                    if(concepts[i].name === "dog"){
+                        message = " Its a dog!";
+                    }
+                }
+                res.render('index',{message: message, url: url});
+            },
+            function(err) {
+                res.render('index',{message: 'Error!' });
+            }
+        );
+    })
+    .post(function (req,res) {
+        console.log("here");
+        let url = req.body.url;
+        console.log(url);
+        ClarifaiApp.models.predict(Clarifai.GENERAL_MODEL, url).then(
+            function (response) {
+                let message = "It's not a dog";
+                console.log(response);
+                let concepts = response.outputs[0].data.concepts;
+                for (let i = 0; i < concepts.length; i++) {
+                    if (concepts[i].name === "dog") {
+                        message = " Its a dog!";
+                    }
+                    console.log("it worked");
+                }
+                res.render('index', {message: message, url: url});
+            },
+            function (err) {
+                console.log(err);
+                res.render('index', {message: 'Error!', url: url});
+            });
+    });
+
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+const ClarifaiApp = new Clarifai.App({
+    apiKey: 'bee8fc08b703468793eaad8f09193b4f'
+});
